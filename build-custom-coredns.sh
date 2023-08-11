481c562f6a2a24393142d6e5776549efe2a128d0
Fan Yang <missdeer@gmail.com>
2023-08-11 09:36:19 +0800

    add https plugin in build prepare scripts

--- build-custom-coredns.sh
+++ build-custom-coredns.sh
@@ -10,7 +10,7 @@ git pull
 cd ../coredns
 git apply ../coredns_custom_build/forward.go.patch
 git apply ../coredns_custom_build/forward-setup.go.patch
-sed -i.bak 's|forward:forward|fallback:github.com/missdeer/fallback\ndnsredir:github.com/leiless/dnsredir\nforward:forward\nproxy:github.com/missdeer/proxy|g' plugin.cfg
+sed -i.bak 's|forward:forward|fallback:github.com/missdeer/fallback\ndnsredir:github.com/leiless/dnsredir\nforward:forward\nproxy:github.com/missdeer/proxy\nhttps:github.com/v-byte-cpu/coredns-https|g' plugin.cfg
 sed -i.bak 's|hosts:hosts|ads:github.com/missdeer/ads\nblocklist:github.com/relekang/coredns-blocklist\nhosts:hosts|g' plugin.cfg
 sed -i.bak 's|rewrite:rewrite|rewrite:rewrite\nbogus:github.com/missdeer/bogus\nipset:github.com/missdeer/ipset|g' plugin.cfg
 sed -i.bak 's|cache:cache|cache:cache\nredisc:github.com/missdeer/redis|g' plugin.cfg
